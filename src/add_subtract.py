a9820ebf59b4b30ac5608153e9f202383c88b199
Jitendra <49111685+dubeyjitendra@users.noreply.github.com>
2024-07-19 14:25:27 +0530

    Update add_subtract.py

--- src/add_subtract.py
+++ src/add_subtract.py
@@ -4,6 +4,12 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
@@ -12,4 +18,10 @@ def test_sub():
     assert sub(5,2) == 3
     assert sub(1,-1) == 2
 
+def test_mul(a,b):
+    assert mul(5,4) == 20
+    assert mul(5,2) == 10
 
+def test_divide(a,b):
+    assert divide(10,2) == 5
+    assert divide(100,10)== 10
