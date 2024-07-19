d19832f8a6c53bcca41e34f15251f0ad9284aa86
Jitendra <49111685+dubeyjitendra@users.noreply.github.com>
2024-07-19 14:21:16 +0530

    Update add_subtract.py

--- src/add_subtract.py
+++ src/add_subtract.py
@@ -1,8 +1,15 @@
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def test_sub():
+    assert sub(5,2) == 3
+    assert sub(1,-1) == 2
 
 
